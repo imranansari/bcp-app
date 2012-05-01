6000dec4f2088e057ab2269307a76ff9d4ad93b8
iansari <imran.iansari@gmail.com>
2012-05-01 18:49:51 -0400

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,103 @@
+window.HomeView = Backbone.View.extend({
+
+    template:_.template($('#home').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page1View = Backbone.View.extend({
+
+    template:_.template($('#page1').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+window.Page2View = Backbone.View.extend({
+
+    template:_.template($('#page2').html()),
+
+    render:function (eventName) {
+        $(this.el).html(this.template());
+        return this;
+    }
+});
+
+var AppRouter = Backbone.Router.extend({
+
+    routes:{
+        "":"home",
+        "page1":"page1",
+        "page2":"page2"
+    },
+
+    initialize:function () {
+        // Handle back button throughout the application
+        $('.back').live('click', function(event) {
+            window.history.back();
+            return false;
+        });
+        this.firstPage = true;
+    },
+
+    home:function () {
+        console.log('#home');
+        this.changePage(new HomeView());
+    },
+
+    page1:function () {
+        console.log('#page1');
+        this.changePage(new Page1View());
+    },
+
+    page2:function () {
+        console.log('#page2');
+        this.changePage(new Page2View());
+    },
+
+    changePage:function (page) {
+        $(page.el).attr('data-role', 'page');
+        page.render();
+        $('body').append($(page.el));
+        console.log(event.target);
+        //var transition = $.mobile.defaultPageTransition;
+        var transition = "none";
+        // We don't want to slide the first page
+        if (this.firstPage) {
+            transition = 'none';
+            this.firstPage = false;
+        }
+        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
+    }
+
+});
+
+$(document).ready(function () {
+    console.log('document ready');
+    app = new AppRouter();
+    Backbone.history.start();
+
+    $("#testButton").live('swipeleft',function(){
+        alert('swipe left');
+    });
+
+    $("#testButton").live('tap',function(){
+        alert('tap');
+    });
+
+    $(".swipableRow").live('swipeleft',function(){
+        //alert('swipe row left');
+        $(".swipableRow").append("<p>test</p>");
+    });
+
+    $(".swipableRow").live('taphold',function(){
+        alert('tap hold');
+
+    });
+
+});
